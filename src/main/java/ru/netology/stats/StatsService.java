9eded6e0e4c6cd4683eaaaecf791155b2009bedf
Elvira Menshikova <em97-97@mail.ru>
2023-03-13 19:01:51 +0400

    Initial commit

---
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -0,0 +1,64 @@
+package ru.netology.stats;
+
+public class StatsService {
+
+    public int calculateSum(int[] sales) {
+        int sum = 0;
+        for (int sale : sales) {
+            sum = sum + sale;
+        }
+        return sum;
+    }
+
+    public int calculateAvg(int[] sales) {
+        return calculateSum(sales) / sales.length;
+    }
+
+    public int sumMaxMonth(long[] sales) {
+        int maxMonth = 0;
+        int month = 0;
+        for (long sale : sales) {
+            if (sale >= sales[maxMonth]) {
+                maxMonth = month;
+            }
+            month = month + 1;
+        }
+        return maxMonth + 1;
+    }
+
+
+    public int sumMinMonth(long[] sales) {
+        int minMonth = 0;
+        int month = 0;
+        for (long sale : sales) {
+            if (sale <= sales[minMonth]) {
+                minMonth = month;
+            }
+            month = month + 1;
+        }
+        return minMonth + 1;
+    }
+
+    public int amountMonthUnderAvg(int[] sales) {
+        int count = 0;
+        int salesAvg = calculateAvg(sales);
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] < salesAvg) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public int amountMonthOverAvg(int[] sales) {
+        int count = 0;
+        int salesAvg = calculateAvg(sales);
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] > salesAvg) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+}
